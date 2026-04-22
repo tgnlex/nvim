f2ce1ca6881a58587c81713fa1cd629d4939e0f0
tgnlex <alexanderwebber76@gmail.com>
2026-04-22 13:44:25 -0400

    auto-commit

--- arch/plugins/lualine.lua
+++ arch/plugins/lualine.lua
@@ -2,6 +2,21 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("lualine").setup()
+    require("lualine").setup({
+      theme = 'gruvbox',
+      options = {
+        icons_enabled = false,
+        component_seperators = '',
+        section_seperators = '',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = { {'filename', path = 1 } },
+        lualine_x = { 'diagnostics' },
+        lualine_y = { 'filetype', 'lsp_status' },
+        lualine_z = { 'location' }
+      }
+    })
   end
 }
---
+++ arch/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+    {
+      "williamboman/mason.nvim",
+      config = function()
+        require("mason").setup()
+      end
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      config = function()
+        require("mason-lspconfig").setup({
+          ensure_installed = {
+            "lua_ls",
+            "rust_analyzer",
+            "clangd",
+            "eslint",
+            "rome",
+            "pyre"
+          }
+        })
+      end
+    }
+}
